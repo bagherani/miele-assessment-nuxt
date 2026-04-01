4df9a8f14be9e7482fc384fd76ad7a9b1a7f35f3
Mohi Bagherani <mohammad.bagherani@gmail.com>
2026-04-01 10:35:03 +0200

    Add Tailwind CSS integration and update project structure

---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+type Props = {
+  message: string
+}
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+  hello {{ message }}
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex min-h-full flex-col">
+    <header class="h-9 bg-blue-400 p-3">
+    </header>
+    <main class="flex-1 p-4 bg-gray-50">
+      <slot />
+    </main>
+    <footer class="text-center bg-blue-100 p-3">All rights reserved</footer>
+  </div>
+</template>
